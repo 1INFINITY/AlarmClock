4e9bf178eedc85c23046ca1f0ac6ffa59ffd65c4
1INFINITY <praecantatio@mail.ru>
2023-04-02 20:34:05 +0300

    added checkbox layout with letters and colors

---
+++ app/src/main/java/ru/mirea/ivashechkinav/alarmclock/CircleCheckBox.kt
@@ -0,0 +1,19 @@
+package ru.mirea.ivashechkinav.alarmclock
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import android.widget.TextView
+
+class CircleCheckBox(context: Context, attrs: AttributeSet): FrameLayout(context, attrs) {
+
+    init {
+        inflate(context, R.layout.circle_checkbox_layout, this)
+
+        val textView: TextView = findViewById(R.id.tvCircleCheckbox)
+
+        val attributes = context.obtainStyledAttributes(attrs, R.styleable.CircleCheckBox)
+        textView.text = attributes.getString(R.styleable.CircleCheckBox_letter)
+        attributes.recycle()
+    }
+}
