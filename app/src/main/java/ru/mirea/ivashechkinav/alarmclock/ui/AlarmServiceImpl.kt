f9855de44545bb01851412a7088499592c102403
1INFINITY <praecantatio@mail.ru>
2023-05-16 02:34:24 +0300

    fixed change invoke time again

--- app/src/main/java/ru/mirea/ivashechkinav/alarmclock/ui/AlarmServiceImpl.kt
+++ app/src/main/java/ru/mirea/ivashechkinav/alarmclock/ui/AlarmServiceImpl.kt
@@ -16,6 +16,8 @@ import ru.mirea.ivashechkinav.alarmclock.domain.DaysOfWeek.Companion.toInt
 import ru.mirea.ivashechkinav.alarmclock.ui.activities.AlarmActivity
 import ru.mirea.ivashechkinav.alarmclock.ui.activities.MainActivity
 import ru.mirea.ivashechkinav.alarmclock.ui.models.AlarmUi
+import kotlin.math.min
+import kotlin.math.abs
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
@@ -105,7 +107,6 @@ class AlarmServiceImpl @Inject constructor(
             calendar.set(Calendar.HOUR, alarmCalendar.get(Calendar.HOUR))
             calendar.set(Calendar.MINUTE, alarmCalendar.get(Calendar.MINUTE))
             calendar.set(Calendar.SECOND, alarmCalendar.get(Calendar.SECOND))
-            //calendar.set(Calendar.MILLISECOND, alarmCalendar.get(Calendar.MILLISECOND))
             nextTimestamp = calendar.timeInMillis + 24 * 3600 * 1000
             return AlarmUi(alarm).copy(invokeTimestamp = nextTimestamp)
         }
@@ -128,10 +129,52 @@ class AlarmServiceImpl @Inject constructor(
         return AlarmUi(alarm).copy(invokeTimestamp = nextTimestamp)
     }
 
+    private fun findNearestAlarm3(alarm: Alarm): Alarm {
+        val DAY_IN_MILLIS = 24 * 3600 * 1000L
+        var daysSet = alarm.daysOfWeek
+
+        val currentCalendar = Calendar.getInstance()
+        var currentMillis = currentCalendar.timeInMillis
+        val alarmMillis = alarm.invokeTimestamp
+        // Command currentCalendar.timeInMillis - currentCalendar.timeInMillis % DAY_IN_MILLIS
+        // show wrong date at 2 am, because of this offset + 3 * 3600 * 1000
+        val currentMillisWithoutDays = (currentCalendar.timeInMillis + 3 * 3600 * 1000) % DAY_IN_MILLIS
+        val alarmMillisWithoutDays = (alarmMillis % DAY_IN_MILLIS+ 3 * 3600 * 1000)
+        val mayBeSameDay = alarmMillisWithoutDays > currentMillisWithoutDays
+        if (daysSet.isEmpty()) {
+            currentMillis += alarmMillisWithoutDays - currentMillisWithoutDays
+            if (mayBeSameDay) return AlarmUi(alarm).copy(invokeTimestamp = currentMillis)
+            currentMillis += DAY_IN_MILLIS
+            return AlarmUi(alarm).copy(invokeTimestamp = currentMillis)
+        }
+
+        var currentDay = currentCalendar.get(Calendar.DAY_OF_WEEK)
+        val currentDayName = DaysOfWeek.fromInt(currentDay)
+        var nextDay = daysSet.find {
+            it.value > currentDayName.value || ((it.value == currentDayName.value) && mayBeSameDay)
+        }?.toInt()
+            ?: daysSet.first().toInt()
+        nextDay = (nextDay + 5) % 7 + 1
+        currentDay = (currentDay + 5) % 7 + 1
+        val daysDifference =
+            if (nextDay == currentDay && mayBeSameDay)
+                0
+            else
+                if (nextDay - currentDay <= 0)
+                    nextDay - currentDay + 7
+                else
+                    nextDay - currentDay
+        val offsetInMillis = daysDifference * DAY_IN_MILLIS
+        currentMillis +=  alarmMillisWithoutDays - currentMillisWithoutDays
+        val nextTimestamp = currentMillis + offsetInMillis
+        return AlarmUi(alarm).copy(invokeTimestamp = nextTimestamp)
+    }
+
+
     override fun selectAlarmDays(alarmId: Long, selectedDays: EnumSet<DaysOfWeek>) {
         GlobalScope.launch {
             val alarm = repository.getAlarmById(alarmId).copy(daysOfWeek = selectedDays)
-            val nearestAlarm = findNearestAlarm2(alarm = alarm)
+            val nearestAlarm = findNearestAlarm3(alarm = alarm)
             repository.updateAlarm(nearestAlarm)
         }
     }
